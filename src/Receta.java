c9c3102ed43d1648b0350026e70cf2e59586c7ac
DiegoAleksis <diegoarh19@gmail.com>
2022-03-14 11:44:50 -0600

    Clase receta avanzada 2

--- src/Receta.java
+++ src/Receta.java
@@ -1,16 +1,33 @@
 import java.util.ArrayList;
 
 public class Receta {
+ private String nombre;
  private double tiempoPreparacion;
  private ArrayList<Ingrediente>ingredientes;
  private ArrayList<String> pasos;
+
+ public Receta(String nombre) {
+  this.nombre = nombre;
+ }
+
+ public String getNombre() {
+  return nombre;
+ }
+
+ public void setNombre(String nombre) {
+  this.nombre = nombre;
+ }
 
  public Receta(double tiempoPreparacion, ArrayList<Ingrediente> ingredientes, ArrayList<String> pasos) {
   this.tiempoPreparacion = tiempoPreparacion;
   this.ingredientes = ingredientes;
   this.pasos = pasos;
+
   System.out.println("Receta creada.");
   }
+
+  public Receta(){}
+
 
  public double getTiempoPreparacion() {
   return tiempoPreparacion;
@@ -36,12 +53,25 @@ public class Receta {
   this.pasos = pasos;
 
  }
- public void mostrarIngredientes(){
-  for (Ingrediente ingrediente :ingredientes) {
-   System.out.println(ingrediente);
-  }
-  for(int i =0; i<ingredientes.size(); i++){
-   System.out.println(ingredientes.get(i));
+ public void mostrarIngredientes() {
+  for (int i = 0; i < ingredientes.size(); i++) {
+   System.out.println(i + 1 + "-" + ingredientes.get(i));
   }
  }
-}
+  public void mostrarPasos() {
+   for (int i = 0; i < pasos.size(); i++) {
+    System.out.println(i + 1 + ". " + pasos.get(i));
+   }
+  }
+   public void mostrarReceta(){
+    System.out.println(nombre);
+    System.out.println(tiempoPreparacion);
+    mostrarIngredientes();
+    mostrarPasos();
+   }
+
+  }
+
+
+
+
--- src/Recetario.java
+++ src/Recetario.java
@@ -1,3 +1,42 @@
-public class Recetario {
+import java.util.ArrayList;
 
+public class Recetario {
+private ArrayList<Receta>recetas;
+private String autor, titulo;
+
+    public Recetario(ArrayList<Receta> recetas, String autor, String titulo) {
+        this.recetas = recetas;
+        this.autor = autor;
+        this.titulo = titulo;
+    }
+
+    public ArrayList<Receta> getRecetas() {
+        return recetas;
+    }
+
+    public void setRecetas(ArrayList<Receta> recetas) {
+        this.recetas = recetas;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void mostrarRecetario(){
+        System.out.println(titulo);
+        System.out.println(autor);
+
+    }
 }
